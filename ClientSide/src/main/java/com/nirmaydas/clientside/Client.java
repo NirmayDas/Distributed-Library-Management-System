a9c2f3c8d9f97d49e0d89d165c9ff7eb2eea41a5
Nirmay Das <nirmaydas99@gmail.com>
2025-04-28 13:21:34 -0500

    Full Distributed Library Management System Project

--- ClientSide/src/main/java/com/nirmaydas/clientside/Client.java
+++ ClientSide/src/main/java/com/nirmaydas/clientside/Client.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//
 public class Client extends Application {    
     @Override
     public void start(Stage primaryStage) throws Exception {
